91eac966071c3ffb19ecb09dab67c4aa7ad3af83
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2018-01-20 13:40:45 -0800

    Highest Product of 3 (Python) [Greedy Algorithm]

---
+++ Highest_Product_of_3/python/test/test_highest_product_of_3.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestHighestProductOf3(unittest.TestCase):
+    """
+    Test the function returning highest product of 3. Negative numbers included.
+    """
+
+    def test_highest_product_of_3(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
